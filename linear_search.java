196a8a9de62631452e8123d5566bfcca27a40dac
Naman9493 <namanchauhan150@gmail.com>
2025-08-24 12:54:21 +0530

    Linear search

---
+++ linear_search.java
@@ -0,0 +1,21 @@
+impor java.util.*;
+public class linear_search {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Give size of your array :");
+          int s = sc.nextInt();
+         System.out.print("Give all elements : ");
+         int arr[]=new int[s];
+        for (int i=0;i<s;i++){
+            arr[i]=sc.nextInt();
+            System.out.print(arr[i]+" ");
+        }
+        System.out.print("Enter Search element: ");
+        int e=sc.nextInt();
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==e){
+                System.out.println("search element is "+e+" on "+i);
+            break;}
+        }
+    }
+}
